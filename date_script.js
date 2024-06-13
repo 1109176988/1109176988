c43dcf547e69aaff4cf394d8a7516a97aeb3f327
fury_123 <73373421+1109176988@users.noreply.github.com>
2024-06-13 20:56:52 +0800

    Update date_script.js

--- date_script.js
+++ date_script.js
@@ -11,12 +11,15 @@ const getFormattedDate = () => {
 // 主执行函数
 const main = () => {
     const lastRunTime = getFormattedDate();
-    const message = `文件最后执行时间: ${lastRunTime}, (每天早上八点半执行一次)`;
+    const message = `文件最后执行时间: ${lastRunTime}, (每天早上八点执行一次)`;
     console.log(message);
 
-    // 可选：将执行时间写入到文件中保存
+    // 将执行时间写入到文件中保存
     const logPath = path.join(__dirname, 'lastRunTime.log');
     fs.writeFileSync(logPath, message);
+
+    // 输出最后执行时间，用于后续 GitHub Actions 步骤
+    console.log(lastRunTime);
 };
 
 main();
